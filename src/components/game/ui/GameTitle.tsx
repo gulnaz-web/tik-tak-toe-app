bf1f599a48b677d12e286fda9a172877ece5d366
yetimol <yetimol@MacBook-Pro-yetimol.local>
2025-10-15 00:26:54 +0300

    refactoring GameTitle

--- src/components/game/ui/GameTitle.tsx
+++ src/components/game/ui/GameTitle.tsx
@@ -1,3 +1,3 @@
-export const GameTitle = () => (
-  <h1 className="text-4xl leading-tight">Крестики нолики</h1>
-);
+export function GameTitle() {
+  return <h1 className="text-4xl leading-tight">Крестики нолики</h1>;
+}
